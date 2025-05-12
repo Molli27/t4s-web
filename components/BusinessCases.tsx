92c4974479efe7f1e4778c2571a5a572e3dc8be0
Marc Mollenkopf <marcmollenkopf64@gmail.com>
2025-05-12 16:24:10 +0200

    update of style

--- components/BusinessCases.tsx
+++ components/BusinessCases.tsx
@@ -4,11 +4,13 @@ const BusinessCases: React.FC = () => {
   return (
     <section
       id="business-cases"
-      className="w-full bg-gradient-to-b from-warm-gray to-white pt-20 pb-16 px-4 sm:px-6 lg:px-8"
+      className="w-full pt-20 pb-16 px-4 sm:px-6 lg:px-8 pb-16 mt-[100px]"
       style={{ background: "linear-gradient(to bottom, #D7D2CB, #FFFFFF)" }}
     >
       <div className="max-w-4xl mx-auto space-y-8 text-black">
-        <h1 className="text-3xl sm:text-4xl font-bold text-blue-dark">Anwendungen</h1>
+        <h1 className="text-3xl sm:text-4xl font-bold text-blue-dark"
+            style={{ color: "#00249C" }}>
+          Anwendungen</h1>
 
         <div className="space-y-4">
           <h2 className="text-xl font-bold">Effizienz steigern – Ressourcen schonen</h2>
@@ -30,7 +32,10 @@ const BusinessCases: React.FC = () => {
               und 230 Artikeln zeigt sich erst durch die t4s-Systeme das volle Optimierungspotenzial.
             </p>
             <Link href="/use-cases/ProduktionsCase">
-              <span className="inline-block text-blue-dark font-bold underline hover:text-blue-cyan transition">
+              <span className="inline-block px-6 py-2 font-semibold rounded-md shadow-md hover:bg-blue-dark hover:text-white transition"
+                style={{ backgroundColor: "#00249C", // deep blue
+                         color: "#FFFFFF", }}
+              >
                 Link zum Bericht
               </span>
             </Link>
@@ -46,7 +51,10 @@ const BusinessCases: React.FC = () => {
               mit unzureichendem Warmwasser. Erst der Einsatz des optischen t4s-Messsystems brachte Klarheit.
             </p>
             <Link href="/use-cases/HeizungsCase">
-              <span className="inline-block text-blue-dark font-bold underline hover:text-blue-cyan transition">
+              <span className="inline-block px-6 py-2 font-semibold rounded-md shadow-md hover:bg-blue-dark hover:text-white transition"
+                style={{ background: "#00249C", // deep blue
+                         color: "#FFFFFF",}}
+              >
                 Link zum Bericht
               </span>
             </Link>
